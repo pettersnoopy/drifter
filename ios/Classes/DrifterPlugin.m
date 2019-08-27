e4f1936a7496867d703009f1201ce5fee9256977
luopeng <15158074877@163.com>
2019-08-27 16:44:02 +0800

    modify swift 2 oc

--- ios/Classes/DrifterPlugin.m
+++ ios/Classes/DrifterPlugin.m
@@ -1,8 +1,30 @@
 #import "DrifterPlugin.h"
-#import <drifter/drifter-Swift.h>
+#import <AdSupport/ASIdentifierManager.h>
 
 @implementation DrifterPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftDrifterPlugin registerWithRegistrar:registrar];
+    FlutterMethodChannel* channel = [FlutterMethodChannel methodChannelWithName:@"me.wener.drifter" binaryMessenger:[registrar messenger]];
+    DrifterPlugin* instance = [[DrifterPlugin alloc] init];
+    [registrar addMethodCallDelegate:instance channel:channel];
 }
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if ([@"getIdfa" isEqualToString:call.method]) {
+        [self getIdfa:call result:result];
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
+}
+
+-(void)getIdfa:(FlutterMethodCall*)call result:(FlutterResult)result {
+    if([[ASIdentifierManager sharedManager] isAdvertisingTrackingEnabled]) {
+        NSUUID *identifier = [[ASIdentifierManager sharedManager] advertisingIdentifier];
+        result([identifier UUIDString]);
+        return;
+    }
+    
+    // Get and return IDFA
+    return result(@"default-idfa");
+}
+
 @end
